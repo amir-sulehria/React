ca8068fa39356614ca399cee11db87ebd9ee71b3
= <=>
2019-05-18 14:02:14 +0500

    1-Component-Lec

--- basic-features/src/App.js
+++ basic-features/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
+/*1.2
+here we see one of two ways of defining react component, we create js class with class keyword and then we 
+inherit from Component class which is from react library, above React import is important for rendering
+anything to dom, the class has a render method which get called by react to render something to screen,
+there is one important job every react component has to do it has to return or render somthing, we 
+can do there things too i.e calculations, reach out to internet etc., at end we export this class 
+as default export of this file, this is es6 feature which simply means if u import this whole file, u'll
+simply import this class because it's default export. the code looking like html is actually jsx
+*/
+class App extends Component {
+
+  render(){
+    return(
+      <div className="App">
         <h1>
           Hi there, this is react app
         </h1>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- basic-features/src/index.js
+++ basic-features/src/index.js
@@ -5,8 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+/*1.1
+here the app component is rendered, into place of the root, usually u render one root component i.e. App.js 
+in this case, and u nest all component ur ap might need in app component, u can also nest components 
+inside components
+*/
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
